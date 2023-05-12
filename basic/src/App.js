ede4d87db82a1b308966f21e7d80ba5c78010c9e
WonJun Yeo <rbgh004@gmail.com>
2023-05-12 17:33:29 +0900

    <feat> rendering test

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,23 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'wonjun.yeo';
+  const list = ['우유', '딸기', '바나나'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <h1>Hello {name}</h1>
+        <ul>
+          {
+            list.map(item => (
+                <li>{item}</li>
+            ))
+          }
+        </ul>
+      </>
   );
 }
 
